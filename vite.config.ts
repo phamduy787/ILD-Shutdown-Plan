8b78a8759e02fd2208bf1885aefcfb8df0095d26
phamduy787 <phamduy787@gmail.com>
2026-09-10 09:37:22 +0700

    Update D1 database ID

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import hostingConfig from "./.openai/hosting.json";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "1e414d3f-77e4-4be4-b9f8-fc58e94858e0";
 
 const { d1, r2 } = hostingConfig;
 
